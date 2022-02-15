7cd5d1f2c137e7785e44e8202a3eb4b0a375827d
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-02-15 14:48:40 -0300

    primeiro-commit

---
+++ js/tela-inicial.js
@@ -0,0 +1,25 @@
+let quizzes = [];
+getQuizzes();
+
+function getQuizzes(){
+    const resposta = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    resposta.then(renderizarQuizzes); 
+    resposta.catch(erroQuizzes);
+}
+function erroQuizzes(erro){
+    console.log('Deu erro ai fera');
+}
+function renderizarQuizzes(quizz){
+    console.log(quizzes);
+    quizzes = quizz.data;    
+    let conteudo = document.querySelector(".todos-quizzes");
+    for(i = 0; i<quizzes.length; i++){
+        conteudo.innerHTML += `    
+                <div class="posts">
+                    <img src="${quizzes[i].image}" alt="">
+                    <span class="posts-titulo">${quizzes[i].title}</span>
+                </div>      
+        `;      
+    }
+}
+    
