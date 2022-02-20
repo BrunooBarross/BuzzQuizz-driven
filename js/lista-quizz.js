d916ea2fc63bc6bd73f8f645ce51e731874c5601
pablodamascenoo <pablofacex@gmail.com>
2022-02-20 11:16:18 -0300

    feat: added renderMyQuizzes function

--- js/lista-quizz.js
+++ js/lista-quizz.js
@@ -4,7 +4,7 @@ let comparadorQuizz = false;
 let indexInicial = 0;
 
 getQuizzes();
-setInterval(getQuizzes, 1000);
+setInterval(getQuizzes, 5000);
 
 function getQuizzes() {
     const resposta = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
@@ -15,6 +15,9 @@ function erroQuizzes(erro) {
     console.log('Deu erro ai fera');
 }
 function renderizarQuizzes(quizz) {
+
+    let ids = JSON.parse(localStorage.getItem('ids')) || []
+
     quizzes = quizz.data;
     let conteudo = document.querySelector(".todos-quizzes");
     if (comparadorQuizz == true) {
@@ -26,25 +29,59 @@ function renderizarQuizzes(quizz) {
             j++;
         }
         for (let i = indexInicial - 1; i >= 0; i--) {
-            conteudo.insertAdjacentHTML('afterbegin', `    
-                <div class="posts" onclick="exibirQuizz('${quizzes[i].id}')">
-                    <img src="${quizzes[i].image}" alt="">
-                    <p class="posts-titulo">${quizzes[i].title}</p>
-                </div>      
-            `);
+            if(!ids.includes(quizzes[i].id)){
+                conteudo.insertAdjacentHTML('afterbegin', `    
+                    <div class="posts" onclick="exibirQuizz('${quizzes[i].id}')">
+                        <img src="${quizzes[i].image}" alt="">
+                        <p class="posts-titulo">${quizzes[i].title}</p>
+                    </div>      
+                `);
+            }
         }
         ultimoQuizz = quizzes[0].id;
     }
     if (comparadorQuizz == false) {
         for (let i = 0; i < quizzes.length; i++) {
-            conteudo.innerHTML += `    
-                <div class="posts" onclick="exibirQuizz('${quizzes[i].id}')">
-                    <img src="${quizzes[i].image}" alt="">
-                    <p class="posts-titulo">${quizzes[i].title}</p>
-                </div>      
-        `;
+            if(!ids.includes(quizzes[i].id)){
+                conteudo.innerHTML += `    
+                    <div class="posts" onclick="exibirQuizz('${quizzes[i].id}')">
+                        <img src="${quizzes[i].image}" alt="">
+                        <p class="posts-titulo">${quizzes[i].title}</p>
+                    </div>      
+                `;
+            }
         }
     }
     ultimoQuizz = quizzes[0].id;
     comparadorQuizz = true;
+
+    renderMyQuizzes()
+}
+
+function renderMyQuizzes(){
+
+    let myQuizzes = document.querySelector(".nav-flex")
+
+    myQuizzes.innerHTML = ''
+
+    let ids = JSON.parse(localStorage.getItem('ids')) || []
+
+    for(let i=0; i<ids.length; i++){
+        for(j=0; j<quizzes.length;j++){
+            if(parseInt(ids[i]) === parseInt(quizzes[j].id)){
+                myQuizzes.innerHTML += `
+                <div class="meus-quizzes" onclick="exibirQuizz('${quizzes[j].id}')">
+                    <img src="${quizzes[j].image}"
+                        alt="">
+                    <span class="posts-titulo">${quizzes[j].title}</span>
+                </div>`
+                break
+            }
+        }
+    }
+
+    if(myQuizzes.querySelectorAll(".meus-quizzes") !== null){
+        myQuizzes.parentNode.classList.remove("disabled")
+        document.querySelector(".nav").classList.add("disabled")
+    }
 }
